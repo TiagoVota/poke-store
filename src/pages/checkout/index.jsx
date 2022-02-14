87ea45dd0aa5236a431fc08777611a639922ec14
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-02-14 10:58:31 -0300

    commit to be able do merge from main

--- src/pages/checkout/index.jsx
+++ src/pages/checkout/index.jsx
@@ -1,10 +1,41 @@
-import Container from '../../components/Container'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import useAuth from '../../hooks/useAuth'
+import * as api from '../../services/api.order'
+import { errorModal, successModal } from '../../factories/modalFactory'
+
+import pageTitles from '../../utils/pageTitles'
+
+import Container from '../../components/ContainerHome'
+import Header from '../shared/header'
+// import PokemonsContainer from './pokemonsContainer'
+import Footer from '../shared/footer'
 
 
-const Checkout = () => {
+const Checkout = ({ orderInfoo }) => {
+	const { products, finalizeDate } = orderInfoo
+	const [orderInfo, setOrderInfo] = useState({})
+	const navigate = useNavigate()
+	const { auth: { token } } = useAuth()
+
+	const handleFinalizeOrder = () => {
+		api.postOrder({ token })
+			.then(({ data }) => {
+				setOrderInfo(data)
+				successModal('Order finalized successfully!')
+				navigate('/')
+			})
+			.catch(({ response: { data }}) => errorModal(data))
+	}
+
 	return (
 		<Container>
-			
+			<Header title={pageTitles['checkout']} />
+
+			{/* <PokemonsContainer pokemonsList={products} /> */}
+
+			<Footer />
 		</Container>
 	)
 }
